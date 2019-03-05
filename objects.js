64db31fc0f55a579419397491a705e858d72245e
TK <zeropercent@hotmail.com>
2019-03-05 19:17:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {Usher: 'Yeah'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, artistName, songTitle})
+  return Object.assign({}, playlist, artistName, songTitle)
 }
