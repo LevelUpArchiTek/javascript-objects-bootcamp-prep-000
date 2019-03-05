0eb4d26144e00b234e18923a0470fd2da4caeea2
TK <zeropercent@hotmail.com>
2019-03-05 19:26:22 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {Usher: 'Yeah'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle})
+  return Object.assign({}, playlist, { [artistName]: songTitle })
 }
 
 function removeFromPlaylist(playlist, artistName){
