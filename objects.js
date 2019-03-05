eb3a522c16bd23ff90a2029fcbdaec9befd18ca6
TK <zeropercent@hotmail.com>
2019-03-05 19:21:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign({}, playlist, artistName, songTitle)
 }
 
-
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName;
 }
